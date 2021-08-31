db663f2740a54d39b8b0bd36c4b652aab091b820
Daniel112205 <daniel_maiten@hotmail.com>
2021-08-30 20:40:21 -0500

    update

--- filesystem/index.js
+++ filesystem/index.js
@@ -1,50 +1,41 @@
 //Imports de los módulos
 const fs = require("fs/promises");
 const path = require("path");
-const readFileUsers = () => {
+const readFileUsers = async () => {
     //Imprimir en consola el arreglo de usuarios
     const file = path.resolve("users.json");
-    fs.readFile(file, 'utf8',(error, data) => {
-        if(error){
-            return console.log(error);
-        }
-        return console.log(data.toString());
-    })
+    try {
+        const data = await  fs.readFile(file, 'utf8');
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    } 
 };
 
-const writeHelloWorld = () => {
+const writeHelloWorld = async() => {
     //Escribir hello world! en el archivo hello.txt
     const data = "hello world!";
     const file = path.resolve("hello.txt");
-    fs.writeFile(file, data, (error) => {
-        if(error){
-            return console.log(error);
-        }else{
-            return console.log("Se ha escrito correctamente en el archivo");
-        }
-    })
+    try {
+        await  fs.writeFile(file, data,"utf8");
+    } catch (error) {
+        console.log(error);
+    } 
 };
 
-const addUser = (username) => {
+const addUser = async (username) => {
     //Agregar un usuario en la lista users.json
-    const file = path.resolve("users.json");
-    fs.readFile(file, 'utf8', function (error, data){
-        if (error){
-            return console.log(error);
-        }else{
-            let value = data.toString();
-            value = JSON.parse(value);
-            value.data.push(username);
-            let str = JSON.stringify(value);
-            fs.writeFile(file, str, function (error){
-                if(error){
-                    return console.log(error);
-                }else{
-                    return console.log("Se ha escrito correctamente en el archivo");
-                }
-            });
-        }
-    });
+    const usersPath = path.resolve("users.json");
+
+    try{
+        //Arreglo de usuarios (JSON)
+        const data = await fs.readFile(usersPath, "utf8");
+        const users = JSON.parse(data); //Convertir de JSON a objeto
+        users.push(username);
+        await fs.writeFile(usersPath, JSON.stringify(users));
+    }catch(error){
+        console.log(error);
+    }
 };
 
 //No hace falta ejecutar las funciones
